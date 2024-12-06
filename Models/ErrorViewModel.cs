737f45b94ddf07b6bec4ffcd69d5f0c27ee7a2f7
Cpftfu <alexseev45s@gmail.com>
2024-12-06 16:59:43 +0300

    push

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebSiteHost.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
